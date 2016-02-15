14ae3f519ef3bf0ada93da2d144cb79f348c1b4a
Sauli Tähkäpää <sauli@vaadin.com>
2016-02-15 10:14:24 +0200

    Add more browsers
    Still need to figure out how to determine when iOS is used so that we can call
    tap() instead of click()

--- visual-test/test.js
+++ visual-test/test.js
@@ -1,61 +1,44 @@
 var gemini = require('gemini');
 
-function captureButton(selector, suite) {
-  suite.setCaptureElements(selector).capture('normal');
-};
-
-function captureButtonWithHoverAndClick(selector, suite) {
-  captureButton(selector, suite);
-
-  suite.capture('hovered', function(actions) {
-      actions.mouseMove(selector);
-    })
-    .capture('pressed', function(actions) {
-      actions.mouseDown(selector);
-    })
-    .capture('clicked', function(actions) {
-      actions.mouseUp(selector);
-    });
-};
-
 gemini.suite('overview', function(suite) {
   suite.setUrl('/index.html').setCaptureElements('#container')
+    .before(function(actions,find) {
+      actions.waitForElementToShow('#input', 3000);
+    })
     .capture('01-initial')
-    .capture('02-input:hover', function(actions, find) {
-      actions.mouseMove(find('#input'));
+    .capture('02-opened', function(actions, find) {
+      actions.tap(find('#toggleIcon'));
     })
-    .capture('03-toggle:hover', function(actions, find) {
-      actions.mouseMove(find('#toggleIcon'));
+    .capture('03-closed+selected', function(actions, find) {
+      actions.tap(find('.item'));
     })
-    .capture('04-opened', function(actions, find) {
-      actions.click(find('#toggleIcon'));
+    .capture('04-opened+selected', function(actions, find) {
+      actions.tap(find('#toggleIcon'));
     })
-    .capture('05-closed+selected', function(actions, find) {
-      actions.click(find('.item'));
-    })
-    .capture('06-opened+selected', function(actions, find) {
-      actions.click(find('#toggleIcon'));
-    })
-    .capture('07-clear:hover', function(actions, find) {
-      actions.mouseMove(find('#clearIcon'));
-    })
-    .capture('08-closed', function(actions, find) {
-      actions.click(find('#clearIcon'));
+    .capture('05-closed', function(actions, find) {
+      actions.tap(find('#clearIcon'));
     });
 });
 
 gemini.suite('overlay', function(suite) {
   suite.setUrl('/index.html').setCaptureElements('vaadin-combo-box-overlay')
-    .capture('01-opened', function(actions, find) {
-      actions.click(find('#toggleIcon'));
+    .before(function(actions,find) {
+      actions.waitForElementToShow('#input', 3000);
     })
-    .capture('02-hover', function(actions, find) {
-      actions.mouseMove(find('.item'));
+    .capture('01-opened', function(actions, find) {
+      actions.tap(find('#toggleIcon'));
+    })
+    .capture('02-focused', function(actions, find) {
+      actions.sendKeys(gemini.ARROW_DOWN);
     });
 });
 
 gemini.suite('icons', function(suite) {
-  suite.setUrl('/index.html').setCaptureElements('#container')
+  suite.skip('ios', 'mouse down not supported by touch devices')
+  .setUrl('/index.html').setCaptureElements('#container')
+    .before(function(actions,find) {
+      actions.waitForElementToShow('#input', 3000);
+    })
     .capture('01-toggle-ripple', function(actions,find) {
       actions.mouseDown(find('#toggleIcon'));
     })
@@ -64,10 +47,30 @@ gemini.suite('icons', function(suite) {
       actions.click(find('.item'));
       actions.click(find('#toggleIcon'));
       actions.mouseDown(find('#clearIcon'));
+    })
+    .after(function(actions, find) {
+      // Firefox has issues when leaving the button down >:-)
+      actions.mouseUp(find('#clearIcon'));
     });
 });
 
 gemini.suite('paper-input-alignment', function(suite) {
   suite.setUrl('/paper-input-alignment.html').setCaptureElements('#container')
+    .before(function(actions,find) {
+      actions.waitForElementToShow('#input');
+    })
     .capture('01-initial');
+});
+
+gemini.suite('hover-styles', function(suite) {
+  suite.skip(['ios'], 'cant hover with touch devices.')
+  .setUrl('/index.html')
+  .setCaptureElements('vaadin-combo-box-overlay')
+  .before(function(actions,find) {
+    actions.waitForElementToShow('#input', 3000);
+  })
+  .capture('01-item:hover', function(actions, find) {
+    actions.click(find('#toggleIcon'));
+    actions.mouseMove(find('.item'));
+  });
 });
